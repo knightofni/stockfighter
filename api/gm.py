798411ca3b7b41fc4e99c51374da44c29dacd410
Nicolas Paris <nicolas.paris@healint.com>
2015-12-26 16:35:10 +0800

    moved stuff from marketmaker to gamemaster. cleaned and commented gamemaster

--- api/gm.py
+++ api/gm.py
@@ -10,16 +10,47 @@ class GameMaster(object):
         Starts / Restarts sessions
         Keeps game info and feeds it to further objects
         from : https://discuss.starfighters.io/t/the-gm-api-how-to-start-stop-restart-resume-trading-levels-automagically/143/2
+
+        Public Methods :
+            - start     : arg : level, string identifying the level. Must be part of _LEVELS class attribute
+            - stop      : stop a level
+            - restart   : restart a level with same stock / venue
+            - completion : Updates level state. Prints completion (number of days in game). Usefull to get extra data
+
     """
-    URL = 'https://www.stockfighter.io/gm'
-    LEVELS = ['first_steps', 'chock_a_block']
+    _URL = 'https://www.stockfighter.io/gm'
+    _LEVELS = ['first_steps', 'chock_a_block']
 
     def __init__(self):
 
         self.headers = {
             'Cookie' : 'api_key={}'.format(API_KEY)
             }
-        self.instanceId = None
+        self._instanceId = None
+
+    """
+        API helpers
+    """
+    def _post(self, url):
+        r = requests.post(url, headers=self.headers)
+        return r.json()
+
+    def _get(self, url):
+        r = requests.get(url, headers=self.headers)
+        return r.json()
+
+    """
+        Updating the GameMaster to know advancement / get extra data
+    """
+    def completion(self):
+        """
+            Updates GameMaster so that we know what is the current trading day
+        """
+        self._update()
+        if self._live:
+            print('{}/{} trading days'.format(self._tradingDay, self._endOfTheWorldDay))
+        else:
+            print('Game closed')
 
     def _flash_level2(self):
         """
@@ -29,53 +60,48 @@ class GameMaster(object):
         if flash:
             i1 = flash.index('$')
             i2 = flash[i1+1:].index('$')
-            self.target_price = float(flash[i1+i2+2:-1])
+            self.target_price_l2 = float(flash[i1+i2+2:-1])
         else:
-            self.target_price = None
+            self.target_price_l2 = None
 
 
     def _update(self):
-        url = self.URL + '/instances/{instanceId}'.format(instanceId=self.instanceId)
+        url = self.URL + '/instances/{instanceId}'.format(instanceId=self._instanceId)
         resp = self._get(url)
-        self.live = resp.get('ok')
-        self.endOfTheWorldDay = resp.get('details').get('endOfTheWorldDay')
-        self.tradingDay = resp.get('details').get('tradingDay')
-        self.status = resp
+        self._live = resp.get('ok')
+        self._endOfTheWorldDay = resp.get('details').get('endOfTheWorldDay')
+        self._tradingDay = resp.get('details').get('tradingDay')
+        self._status = resp
         self._flash_level2()
 
 
-    def _post(self, url):
-        r = requests.post(url, headers=self.headers)
-        return r.json()
-
-    def _get(self, url):
-        r = requests.get(url, headers=self.headers)
-        return r.json()
-
+    """
+        Controls the GameMaster
+    """
     def start(self, level):
-        if level not in self.LEVELS:
-            raise Exception('Available levels are : {}'.fornat(self.LEVELS))
+        if level not in self._LEVELS:
+            raise Exception('Available levels are : {}'.fornat(self._LEVELS))
 
-        url = self.URL + '/levels/{level}'.format(level=level)
+        url = self._URL + '/levels/{level}'.format(level=level)
         resp = self._post(url)
         self.account = resp.get('account')
-        self.instanceId = resp.get('instanceId')
+        self._instanceId = resp.get('instanceId')
         self.tickers = resp.get('tickers')
         self.venues = resp.get('venues')
-        self.start_resp = resp
+        self._start_resp = resp
         print('GameMaster : level {} initiated'.format(level))
 
     def stop(self):
-        if self.instanceId is not None:
-            url = self.URL + '/instances/{instanceId}/stop'.format(instanceId=self.instanceId)
+        if self._instanceId is not None:
+            url = self._URL + '/instances/{instanceId}/stop'.format(instanceId=self._instanceId)
             resp = self._post(url)
             print('Stopped')
         else:
             raise Exception('Cant stop because there is no recorded instanceId')
 
     def restart(self):
-        if self.instanceId is not None:
-            url = self.URL + '/instances/{instanceId}/restart'.format(instanceId=self.instanceId)
+        if self._instanceId is not None:
+            url = self._URL + '/instances/{instanceId}/restart'.format(instanceId=self._instanceId)
             resp = self._post(url)
             print('Restarted')
         else:
